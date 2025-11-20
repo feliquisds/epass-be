f523701b3e86f26c279fa0a54ca58c1f3f5ab830
Kauê Souza <161787698+kaue-souza7@users.noreply.github.com>
2025-11-20 15:36:21 -0300

    [fix]. removing unused imports

--- src/main/java/com/epass/epass_be/service/UsuarioService.java
+++ src/main/java/com/epass/epass_be/service/UsuarioService.java
@@ -5,7 +5,6 @@ import com.epass.epass_be.repository.UsuarioRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UsuarioService {
